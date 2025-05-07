9289869586786f6f578ceb27e6d48d7ab1977931
Master <alsrudtjq503@gmail.com>
2025-05-07 09:23:03 +0900

    Project Start

---
+++ myStage/src/main/java/com/mystage/mystage/MyStageApplication.java
@@ -0,0 +1,13 @@
+package com.mystage.mystage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyStageApplication.class, args);
+    }
+
+}
